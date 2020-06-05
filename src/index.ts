76261d29d53ac943aa04881fc96362b1466c1cdc
jinseok.oh <freejin4802@gmail.com>
2020-06-06 01:56:35 +0900

    eslint 및 prettier 셋팅

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-app.get("/", (req, res) => { res.send("sended");})
+app.get("/", (req, res) => {
+  res.send("sended");
+});
 app.listen(4000, () => console.log("start"));
+
+function f() {
+  console.log("tt");
+  const b = 4;
+  const a = b
+    ? {
+        c: "c",
+      }
+    : {};
+  console.log(a);
+}
+
+f();
